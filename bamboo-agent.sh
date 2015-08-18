6c8a47234b0288aa5ba81e08621059a0d1c607be
Jacek Kowalski <Jacek@jacekk.info>
2015-08-18 21:40:18 +0200

    Add encoding-related environment variables

--- bamboo-agent.sh
+++ bamboo-agent.sh
@@ -22,6 +22,9 @@ if [ ! -f bamboo-agent-home/bamboo-agent.cfg.xml -a "${BAMBOO_AGENT_UUID}" != ""
 	echo 'agentUuid='${BAMBOO_AGENT_UUID} > bamboo-agent-home/uuid-temp.properties
 fi
 
+echo "Setting up the environment..."
+export LANG=en_US.UTF-8
+export JAVA_TOOL_OPTIONS="-Dfile.encoding=utf-8 -Dsun.jnu.encoding=utf-8"
 export DISPLAY=:1
 
 echo Starting Xvfb...
